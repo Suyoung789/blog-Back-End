b6f8865c95114db6aa36495b3e9dafad89dd5560
Suyoun789 <rsy011203@gmail.com>
2019-04-15 16:04:39 +0900

    카테고리 get 구현

--- Server/app/views/post/category.py
+++ Server/app/views/post/category.py
@@ -1,5 +1,5 @@
 from flask_restful import Api
-from flask import Blueprint, Response, request
+from flask import Blueprint, Response, request, jsonify
 
 from app.views import BaseResource
 from app.models.post import CategoryModel
@@ -9,6 +9,9 @@ api = Api(Blueprint(__name__, __name__))
 
 @api.resource('/category')
 class Category(BaseResource):
+    def get(self):
+        return jsonify([{'category_name': Category.name}
+                        for Category in CategoryModel.objects()])
     def post(self):
         '''
         카테고리 추가
